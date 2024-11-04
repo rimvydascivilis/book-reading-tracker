a29c78d66295c61cb19c3b984b1abdcf0b17c9ef
Rimvydas Čivilis <71722408+rimvydascivilis@users.noreply.github.com>
2024-11-04 20:56:43 +0200

    Add base url env var

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {getToken} from "../service/TokenService";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8081/api",
   timeout: 10000, // 10 seconds
 });
 
